d5ac8cc266fb4646b6c12c00f82bcab1f41265d9
QuantumHero <dani.bg97@web.de>
2017-04-03 04:11:13 +0200

    Added new possibility of custom bossdrops

---
+++ src/tModLoader/NinMod/Items/TestItems/ModGlobalItem.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace NinMod.Items.TestItems {
+    public class ModGlobalItem : GlobalItem {
+
+        public override void OpenVanillaBag(string context, Player player, int arg) {
+            if(context == "bossBag" && arg == 3324) {
+                //TODO add new boss drop in Wall Of Flesh Tresure Bag
+//                Item.NewItem((int)player.position.X, (int)player.position.Y, player.width, player.height, ItemID.DirtBlock);
+            }
+        }
+    }
+}
