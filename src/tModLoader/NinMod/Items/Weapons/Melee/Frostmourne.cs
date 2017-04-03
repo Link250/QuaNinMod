f203afcc64724f24e594245976b9e260ec16406f
ultrasn0w <r.barth@dshack.org>
2017-04-03 19:49:57 +0200

    sry nochmal

--- src/tModLoader/NinMod/Items/Weapons/Melee/Frostmourne.cs
+++ src/tModLoader/NinMod/Items/Weapons/Melee/Frostmourne.cs
@@ -27,7 +27,7 @@ namespace NinMod.Items.Weapons.Melee {
     }
 
     public override void GetWeaponDamage(Player player, ref int damage){
-      damage += player.statLife / 2;
+      damage = item.damage + player.statLife / 2;
     }
   }
 }
