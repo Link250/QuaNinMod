f6101c996e9256cd327e4dcf44a4def9fc7a6fc9
QuantumHero <dani.bg97@web.de>
2017-04-05 20:58:48 +0200

    advanced sentry AI

--- src/tModLoader/NinMod/Projectiles/CustomSentryProj.cs
+++ src/tModLoader/NinMod/Projectiles/CustomSentryProj.cs
@@ -29,49 +29,40 @@ namespace NinMod.Projectiles
  
         public override void Kill(int timeLeft)
         {
- 
             Main.PlaySound(28, projectile.Center, 62);  
- 
-           }
+        }
  
         public override void AI()
         {
- 
-            
-            projectile.rotation += 0.1f;  
- 
-            for (int i = 0; i < 200; i++)
-            {
-                NPC target = Main.npc[i];
- 
-       
-                float shootToX = target.position.X + (float)target.width * 0.5f - projectile.Center.X;
-                float shootToY = target.position.Y + (float)target.height * 0.5f - projectile.Center.Y;
-                float distance = (float)System.Math.Sqrt((double)(shootToX * shootToX + shootToY * shootToY));
- 
-               
-                if (distance < 720f && !target.friendly && target.active) 
-                {
-                    if (projectile.ai[0] > 12f)
-                    {
-                        
-                        distance = 3.2f / distance;
- 
-                        
-                        shootToX *= distance * 3;
-                        shootToY *= distance * 3;
-                        int damage = 5;  
-						
-                        Projectile.NewProjectile(projectile.Center.X, projectile.Center.Y, shootToX, shootToY, 14, damage, 0, Main.myPlayer, 0f, 0f); 
-                        Main.PlaySound(28, (int)projectile.position.X, (int)projectile.position.Y, 24); 
-                        projectile.ai[0] = 0f;
+            projectile.rotation += 0.1f;
+
+            if(projectile.ai[0] > 5f) {
+                float closestDistance = 720f;
+                NPC closestNPC = null;
+                foreach (NPC npc in Main.npc) {
+                    float distance = (projectile.Center - npc.Center).Length();
+                    if (distance <= closestDistance && !npc.friendly && npc.active && npc.life > 0) {
+                        closestDistance = distance;
+                        closestNPC = npc;
                     }
                 }
-				
+                if (closestNPC != null) {
+                    float bulletSpeed = 10;
+                    // aiming
+                    float bulletFlyTime = closestDistance / bulletSpeed / 2;
+                    Vector2 FutureEnemyPos = closestNPC.Center + closestNPC.velocity * bulletFlyTime;
+                    Vector2 angle = FutureEnemyPos - projectile.Center;
+                    angle = angle * bulletSpeed / angle.Length();
+                    //randomization
+                    angle = angle.RotatedByRandom(3.141/100);
+                    //shooting
+                    Projectile.NewProjectile(projectile.Center, angle, 14, 5, 0, Main.myPlayer, 0f, 0f);
+                    Main.PlaySound(28, (int)projectile.position.X, (int)projectile.position.Y, 24);
+                    projectile.ai[0] = 0f;
+                }
+            } else {
+                projectile.ai[0] += 1f;
             }
-			
-            projectile.ai[0] += 1f;
- 
         }
     }
 }
