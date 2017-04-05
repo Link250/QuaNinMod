6a8f6890498510af0cdb1bc4c8092a4ede8a880f
ultrasn0w <r.barth@dshack.org>
2017-04-05 14:16:04 +0200

    try to fix

--- src/tModLoader/NinMod/Items/Weapons/Holy/Lifebinder.cs
+++ src/tModLoader/NinMod/Items/Weapons/Holy/Lifebinder.cs
@@ -9,12 +9,12 @@ namespace NinMod.Items.Weapons.Holy {
   public class Lifebinder : CustomItem {
     public override void SetDefaults() {
 			item.name = "The Lifebinder";
-      this.holy = true;
+            this.holy = true;
 			item.damage = 12;
-      item.mana = 3;
-			item.width = 23;
+            item.mana = 3;
+            item.width = 23;
 			item.height = 23;
-      item.toolTip = "Heals for " + item.damage + " HP";
+            item.toolTip = "Heals for " + item.damage + " HP";
 			item.toolTip2 = "A gift from the keeper Freya, creator of the Emerald Dream and protector of all living things.";
 			item.useTime = 39;
 			item.useAnimation = 39;
