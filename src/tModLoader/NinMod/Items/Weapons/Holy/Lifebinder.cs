a1b3fcfa89a1abfa4e939509187005e850db1411
ultrasn0w <r.barth@dshack.org>
2017-04-05 15:07:51 +0200

    formatting

--- src/tModLoader/NinMod/Items/Weapons/Holy/Lifebinder.cs
+++ src/tModLoader/NinMod/Items/Weapons/Holy/Lifebinder.cs
@@ -3,33 +3,32 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-using NinMod.Items;
 
 namespace NinMod.Items.Weapons.Holy {
-  public class Lifebinder : CustomItem {
-    public override void SetDefaults() {
-			item.name = "The Lifebinder";
-            this.holy = true;
-			item.damage = 12;
+    public class Lifebinder : ModItem {
+        public override void SetDefaults() {
+            item.name = "The Lifebinder";
+            item.damage = 12;
             item.mana = 3;
             item.width = 23;
-			item.height = 23;
+            item.height = 23;
             item.toolTip = "Heals for " + item.damage + " HP";
-			item.toolTip2 = "A gift from the keeper Freya, creator of the Emerald Dream and protector of all living things.";
-			item.useTime = 39;
-			item.useAnimation = 39;
-			item.useStyle = 1;
-			Item.staff[item.type] = true;
-			item.noMelee = true;
-			item.value = 000010;
-			item.rare = 5;
-			item.UseSound = SoundID.Item20;
-			item.autoReuse = true;
-		}
+            item.toolTip2 = "A gift from the keeper Freya, creator of the Emerald Dream and protector of all living things.";
+            item.useTime = 39;
+            item.useAnimation = 39;
+            item.useStyle = 1;
+            Item.staff[item.type] = true;
+            item.noMelee = true;
+            item.value = 000010;
+            item.rare = 5;
+            item.UseSound = SoundID.Item20;
+            item.autoReuse = true;
+        }
 
-    public override void GetWeaponDamage(Player player, ref int damage) {
-      base.GetWeaponDamageCustom(player, ref damage, this);
-      item.toolTip = "Heals for " + damage + " HP";
+        public override void GetWeaponDamage(Player player, ref int damage) {
+            CustomPlayer modPlayer = player.GetModPlayer<CustomPlayer>(mod);
+            damage = (int) (modPlayer.holyPower * damage);
+            item.toolTip = "Heals for " + damage + " HP";
+        }
     }
-  }
 }
