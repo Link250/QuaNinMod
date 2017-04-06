e56550658ab949e1f3659af5ebcd56567276f804
Nin-Sama <nincreeper@googlemail.com>
2017-04-06 19:21:07 +0200

    GRINDER IS READY TO GRIND texture not done

---
+++ src/tModLoader/NinMod/Items/Weapons/Summon/Grinder.cs
@@ -0,0 +1,62 @@
+﻿using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+using Terraria.ModLoader;
+ 
+ 
+namespace NinMod.Items.Weapons.Summon 
+{
+    public class Grinder : ModItem
+    {
+ 
+        public override void SetDefaults()
+        {
+            item.name = "Grinder Staff"; 
+            item.damage = 56; 
+            item.mana = 12;  
+            item.width = 56; 
+            item.height = 56;   
+            item.useTime = 55;  
+            item.useAnimation = 55; 
+            item.useStyle = 1;  
+            AddTooltip("Summons a Grinder that grinds enemies, and also Items.");  
+            item.noMelee = true; 
+            item.knockBack = 2.5f;  
+            item.value = Item.buyPrice(0, 10, 0, 0); 
+            item.rare = 4;  
+            item.UseSound = SoundID.Item44; 
+            item.autoReuse = true;
+            item.shoot = mod.ProjectileType("Grinderproj");   
+            item.summon = true;  
+            item.sentry = true; 
+        }
+ 
+        public override bool Shoot(Player player, ref Microsoft.Xna.Framework.Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            Vector2 SPos = Main.screenPosition + new Vector2((float)Main.mouseX, (float)Main.mouseY);  
+            position = SPos;
+            for (int l = 0; l < Main.projectile.Length; l++)
+            {                                                                  
+                Projectile proj = Main.projectile[l];
+                if (proj.active && proj.type == item.shoot && proj.owner == player.whoAmI)
+                {
+                    proj.active = false;
+                }
+            }
+            return true;
+			
+        }
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(98, 1);
+            recipe.AddIngredient(117, 12);
+            recipe.AddIngredient(3619, 1);
+            recipe.AddIngredient(530, 20);
+            recipe.AddIngredient(97, 100);
+            recipe.SetResult(this);
+			recipe.AddTile(221);
+			recipe.AddRecipe();
+		}
+    }
+}
---
+++ src/tModLoader/NinMod/Projectiles/Grinderproj.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+ 
+namespace NinMod.Projectiles  
+ 
+{
+    public class Grinderproj : ModProjectile
+    {
+ 
+        public override void SetDefaults()
+        {
+ 
+            projectile.name = "Gaygrinder"; 
+            projectile.width = 76;
+            projectile.damage = 56;
+            projectile.height = 76; 
+            projectile.hostile = false;   
+            projectile.friendly = true;   
+            projectile.ignoreWater = true;  
+            Main.projFrames[projectile.type] = 1; 
+            projectile.timeLeft = 3000;  
+            projectile.penetrate = -1; 
+            projectile.tileCollide = false; 
+            projectile.sentry = true; 
+        }
+ 
+        public override void Kill(int timeLeft)
+        {
+            Main.PlaySound(28, projectile.Center, 62);  
+        }
+ 
+        public override void AI()
+        {
+            projectile.rotation += 0.1f;
+
+           /* if(projectile.ai[0] > 5f) {
+                float closestDistance = 720f;
+                NPC closestNPC = null;
+                foreach (NPC npc in Main.npc) {
+                    float distance = (projectile.Center - npc.Center).Length();
+                    if (distance <= closestDistance && !npc.friendly && npc.active && npc.life > 0) {
+                        closestDistance = distance;
+                        closestNPC = npc;
+                    }
+                }
+                if (closestNPC != null) {
+                    float bulletSpeed = 10;
+                    // aiming
+                    float bulletFlyTime = closestDistance / bulletSpeed / 2;
+                    Vector2 FutureEnemyPos = closestNPC.Center + closestNPC.velocity * bulletFlyTime;
+                    Vector2 angle = FutureEnemyPos - projectile.Center;
+                    angle = angle * bulletSpeed / angle.Length();
+                    //randomization
+                    angle = angle.RotatedByRandom(3.141/100);
+                    //shooting
+                    Projectile.NewProjectile(projectile.Center, angle, 14, 5, 0, Main.myPlayer, 0f, 0f);
+                    Main.PlaySound(28, (int)projectile.position.X, (int)projectile.position.Y, 24);
+                    projectile.ai[0] = 0f;
+                }
+            } else {
+                projectile.ai[0] += 1f;
+            }
+        */}
+    }
+}
