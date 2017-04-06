ebfd0a8629fdc875adda75422dadc042a11998c9
ultrasn0w <r.barth@dshack.org>
2017-04-06 03:51:01 +0200

    Lifebinder functional, but needs nerf & particles

--- src/tModLoader/NinMod/Items/Weapons/Holy/Bandage.cs
+++ src/tModLoader/NinMod/Items/Weapons/Holy/Bandage.cs
@@ -33,7 +33,6 @@ namespace NinMod.Items.Weapons.Holy {
         }
 
     public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
-        CustomPlayer modPlayer = player.GetModPlayer<CustomPlayer>(mod);
         if (damage > 0) {
             player.statLife += damage;
             if (Main.myPlayer == player.whoAmI) {
--- src/tModLoader/NinMod/Items/Weapons/Holy/Lifebinder.cs
+++ src/tModLoader/NinMod/Items/Weapons/Holy/Lifebinder.cs
@@ -3,14 +3,16 @@ using Microsoft.Xna.Framework;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using NinMod;
 using NinMod.Items;
+using System.Collections.Generic;
 
 namespace NinMod.Items.Weapons.Holy {
   public class Lifebinder : CustomItem {
     public override void SetDefaults() {
 			item.name = "The Lifebinder";
             this.holy = true;
-            item.damage = 12;
+            item.damage = 20;
             item.mana = 3;
             item.width = 23;
 			item.height = 23;
@@ -25,10 +27,32 @@ namespace NinMod.Items.Weapons.Holy {
 			item.rare = 5;
 			item.UseSound = SoundID.Item20;
 			item.autoReuse = true;
+            item.shoot = 14;
+			item.shootSpeed = 40.0f;
 		}
 
     public override void GetWeaponDamage(Player player, ref int damage) {
       base.GetWeaponDamage(player, ref damage);
+    }
+
+    public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack) {
+        if (damage > 0) {
+            Vector2 mouse = Main.screenPosition + new Vector2(Main.mouseX, Main.mouseY);
+            List<Player> possibleTargets = ModHelper.getPlayersInArea(mouse, 125);
+            if(possibleTargets.Count > 0) {
+                foreach (Player pl in possibleTargets) {
+                    if(pl.statLife < pl.statLifeMax2) {
+                        pl.statLife += damage;
+                        pl.HealEffect(damage, true);
+                        if (pl.statLife > pl.statLifeMax2) {
+                            pl.statLife = pl.statLifeMax2;
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+        return false;
     }
   }
 }
